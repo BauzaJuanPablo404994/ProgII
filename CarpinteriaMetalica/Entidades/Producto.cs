9926459bd46566f32f451a8a681d200aa565a6af
Juan Pablo Bauza <404994@tecnicatura.frc.utn.edu.ar>
2023-08-20 14:43:33 -0300

    primer commit

---
+++ CarpinteriaMetalica/Entidades/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpinteriaMetalica.Entidades
+{
+    internal class Producto
+    {
+        public int ProductoNro { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public bool Activo { get; set; }
+
+        public Producto(int productoNumero, string nombre, double precio)
+        {
+            ProductoNro = productoNumero;
+            Nombre = nombre;
+            Precio = precio;
+            Activo = true;
+        }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+
+
+    }
+    
+}
